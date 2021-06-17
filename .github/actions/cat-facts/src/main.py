820740e50a2bcd8307189b72ca366385e2661cf9
Leonty Chudinov <leonty@inbox.ru>
2021-06-17 07:58:21 +0500

    Fix python action

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -19,7 +19,7 @@ fact_list.append(r_obj_list["title"])
 # Select a random fact from the fact_list and return it
 # into a variable named random_fact so we can use it
 def select_random_fact(fact_arr):
-    return fact_arr[random.randint(0, len(fact_list)+1)]
+    return fact_arr[random.randint(0, len(fact_list))]
 
 random_fact = select_random_fact(fact_list)
 
